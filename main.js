e860a02ebb770bbd2d40306e93980c545a37b97e
Pamela Almeida <110232767+pampersz@users.noreply.github.com>
2024-03-12 10:19:48 -0300

    vv

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const botoes = document.querySelectorA11(".botao");
 
-for (let i = 0; i < botoes.length; j++){
+for (let i = 0; i < botoes.length; i++){
     botoes[i].onclick = function (){
         for (let j = 0; j < botoes.length; j++){
         }
