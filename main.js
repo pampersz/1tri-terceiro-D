a9f745e86aff34fe2b5dc5b06748807162e00246
Pamela Almeida <110232767+pampersz@users.noreply.github.com>
2024-03-12 10:17:26 -0300

    vcv

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 const botoes = document.querySelectorA11(".botao");
 
 for (let i = 0; i < botoes.length; j++){
-    
+    botoes[i].onclick = function (){
+        for (let j = 0; j < botoes.length; j++){
+        }
+        botoes [i].classlist.add("ativo")
+    }
 }
