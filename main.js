7ab49350537e2d748d1ddae5f4022f5a0bf5dfaf
Pamela Almeida <110232767+pampersz@users.noreply.github.com>
2024-04-16 11:00:07 -0300

    ng

--- main.js
+++ main.js
@@ -21,4 +21,6 @@ function calculaTempo(tempoObjetivo1){
     let horas = Math.floor(minutos / 60);
     let dias = Math.floor(horas / 24);
 
+    return dias + "dias" + horas + "horas" + minutos + "minutos" + segundos + "segundos";
+
 }
