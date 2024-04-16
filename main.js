08a05aee563e2de57cb880f66a01c650a93553c4
Pamela Almeida <110232767+pampersz@users.noreply.github.com>
2024-04-16 10:25:15 -0300

    fd

--- main.js
+++ main.js
@@ -12,4 +12,6 @@ for (let i = 0; i < botoes.length; i++){
     }
 }
 const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 =  new Date("2024-12-01");
+let tempoAtual = new Date();
 contadores[0].textContent = "contagem regressiva";
