4622a73af5e55f8593bcc8c6ef8a1154d530a145
Pamela Almeida <110232767+pampersz@users.noreply.github.com>
2024-04-16 10:44:32 -0300

    fd

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ for (let i = 0; i < botoes.length; i++){
 }
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 =  new Date("2024-12-01T00:00:00");
-let tempoAtual = new Date();
-
-contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+function calculaTempo(tempoObjetivo1){
+    let tempoAtual = new Date();
+    contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+}
