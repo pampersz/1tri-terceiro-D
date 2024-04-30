4d643856af8d3bfb3692c332471a654cc900a7a1
Pamela Almeida <110232767+pampersz@users.noreply.github.com>
2024-04-30 10:42:31 -0300

    fdgf

--- main.js
+++ main.js
@@ -20,6 +20,9 @@ const tempoObjetivo4 =  new Date("2024-12-01T23:59:59");
 const tempos = [
     tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4
 ]
+for (let i=0; i< contadores.length; i++){
+    contadores[i].textContent = calculaTempo(tempos[i]);
+}
 function calculaTempo(tempoObjetivo1){
     let tempoAtual = new Date();
     contadores[0].textContent = tempoObjetivo1 - tempoAtual;
